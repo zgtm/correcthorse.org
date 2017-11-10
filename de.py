881a0212ef848657752dfe3db3fc96831d91f279
Robert Czechowski <czechowski@bwinf.de>
2017-11-10 18:21:33 +0100

    complete DE translation

--- de.py
+++ de.py
@@ -4,60 +4,79 @@ strings = {
     'compact': 'Kompakte Version',
     'more_options': 'Zusätzliche Optionen',
     'about': 'Über',
-    'subtitle': 'Passwords done right', 
+    'subtitle': 'Passwörter richtig gemacht',
     'title_your_password': 'Dein Correct-Horse-Password',
     'subtitle_generate_password': 'Klicke auf den Button um ein neues zu erzeugen',
     'generate': 'Neues erzeugen',
     'want_more_options': 'Ich brauche <a href="options.html">mehr Einstellungen …</a>',
-    'generator_words':  'Das Passwort wird aus den folgenden Wörtern gebildet:',
-    'title_good_password': 'Why is this a good password?',
-    'subtitle_good_password': 'What makes a good password?',
-    'text_good_password': '''<p>The password above contains 52 bits of entropy.
-This means, it is as random as the result of throwing 52 coins in a 'row. The
-reason for that is because every word has been randomly selected from a list of
-over 8192 distinct words—corresponding to an entropy of 13 bits.</p>
-<p>That means, it is as random, as a completely random password of length 8
-generated from uppercase letters, lowercase letters and numbers.
-<strong>However</strong> it is much more memorable than a complete random
-password of 8 characters. Or as Randal Munroe puts it:</p>''',
-    'reference_xkcd': '''<p>This is taken from Randall Munroes famous carton
-<a href="https://xkcd.com/">XKCD (xkcd.com)</a>.</p>''',
-    'title_trust': 'Why can I trust this page …',
-    'subtitle_trust': ' … with something such important as my passwort?',
-    'text_trust': '''<p>Short answer: You shouldn't. You shouldn't trust
-<strong>any</strong> website with that.</p>
-<p>However, this page is made as simple as possible (from the code point of
-view). So you can convince yourself that</p>
-<ul>
-    <li>the password is generated completely random (as far as your browser
-    implements Math.random())</li>
-    <li>the password is not send to the server</li>
-    <li>nothing else is done except for the password generation</li>
-</ul>
-<p>Because the password is never sent to any server, also nobody in between
-could have read the password above (except from the coworker standing behind
-you). In order to make sure, that also this web page has not been modified at
-all, you should check that you connected this website securely via https and
-the connection is using a trusted certificate.</p>
-<p>By viewing the source of this website, you can see that the only code
-inserted (at the bottom of the page) is the snippet you can see below. There
-is no JS-Framework use, no ads are embedded, neither is google analyics,
-there is no facebook-botton, nothing. Feel free to share this page on facebook
-anyway! :) </p>''',
+    'generator_words':  'Das Passwort wird aus den folgenden Wörtern generiert:',
+    'title_good_password': 'Warum ist das ein gutes Passwort?',
+    'subtitle_good_password': 'Was macht ein gutes Passwort aus?',
+    'text_good_password':
+      '''<p>
+      Das Passwort oben enthält 52 Bit Entropie. Das bedeutet, dass es so zufällig ist, wie das Ergebnis des Werfens von 52 Münzen nacheinander.
+      Das ist so, weil jedes Wort zufällig aus einer Liste von über 8192 verschiedenen Wörtern ausgewählt wurde – was einer Entropie von 13 Bits entspricht.
+      </p>
+      <p>
+      Das heißt, es ist so zufällig wie ein komplett zufälliges Passwort der Länge 8, das aus Großbuchstaben, Kleinbuchstaben und Zahlen generiert wird.
+      <strong>Allerdings</strong> ist es viel einfacher zu merken als ein komplettes zufälliges Passwort von 8 Zeichen.
+      Oder wie Randal Munroe es ausdrückt:
+      </p>''',
+    'reference_xkcd':
+      '''<p>
+      Von Randall Munroes berühmtem Webcartoon
+      <a href="https://xkcd.com/">XKCD (xkcd.com).</a>
+      </p>''',
+    'title_trust': 'Warum kann ich dieser Seite vertrauen …',
+    'subtitle_trust': ' … mit so etwas Wichtigem wie meinem Passwort?',
+    'text_trust':
+      '''<p>
+      Kurze Antwort:
+      Das sollten Sie nicht.
+      Sie sollten damit <strong>keiner</strong> Website vertrauen.
+      </p>
+      <p>
+      Diese Seite ist jedoch so einfach wie möglich gehalten (vom Quellcode her).
+      Sie können sich also davon überzeugen, dass
+      <ul>
+        <li>das Passwort wird komplett zufällig generiert (soweit wie Ihr Browser Math.random() implementiert),</li>
+        <li>das Passwort wird nicht an den Server gesendet,</li>
+        <li>nichts anderes außer der Passwort-Generierung gemacht wird.</li>
+      </ul>
+      </p>
+      <p>
+      Da das Passwort niemals an einen Server geschickt wird, kann auch niemand zwischendurch das Passwort lesen (außer der Kollege, der hinter Ihnen steht).
+      Um sicherzustellen, dass auch diese Webseite überhaupt nicht verändert wurde, sollten Sie überprüfen, dass Sie diese Webseite sicher über HTTPS abgerufen haben und die Verbindung mit einem vertrauenswürdigen Zertifikat hergestellt wurde.
+      </p>
+      <p>
+      Wenn Sie den Quelltext dieser Website betrachten, können Sie sehen, dass der einzige Code, der am Ende der Seite eingefügt wurde, das kleine Snippet ist, das Sie unten sehen können.
+      Es gibt keinen JS-Framework, keine Werbung, die eingebettet ist, keine Google Analytics und es gibt keinen Facebook-Button, garnichts.
+      Aber zögern Sie nicht, trotzdem diese Seite auf Facebook zu teilen, wenn Sie Ihnen gefällt! :)
+      </p>''',
     'title_wordlist': 'Welche Wörter werden verwendet?',
-    'subtitle_wordlist': 'Kann ich die auch herunterladen?',
-    'text_wordlist': '''<p>Die Wortlisten können hier heruntergeladen werden:</p>''',
-    'reference_wordlist': '''<p>Die Wortlisten stammen von dem 
-<a href="http://wortschatz.uni-leipzig.de/html/wliste.html">Wortschatz-Projekt</a>
-Der Universität Leipzig. Sie sind nach den
-<a href="http://wortschatz.uni-leipzig.de/use.html">Wortschatz-Nutzungsbedingungen</a>
-unter <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC-BY-NC</a>
-Lizenz verfügbar. Wörter, die andere Zeichen als Buchstaben enthalten und sehr
-kurze Wörter wurden herausgefiltert. Außerdem wurden alle Wörter in
-Kleinschreibung umgewandelt..</p>
-<p>Die Wortlisten können auch verwendet werden, um sich auf dem eigenen Rechner
-selbst ein Passwort zu erzeugen. Unter Linux kann das folgenden Kommando dafür
-benutzt werden (&lt;file&gt; durch die entsprechende Datei ersetzen):</p>''',
+    'subtitle_wordlist': 'Kann ich die Wortlisten auch herunterladen?',
+    'text_wordlist':
+      '''<p>
+      Die verwendeten Wortlisten können hier heruntergeladen werden:
+      </p>''',
+    'reference_wordlist':
+      '''<p>
+      Die Wortlisten stammen aus dem
+      <a href="http://wortschatz.uni-leipzig.de/html/wliste.html">Wortschatz-Projekt</a>
+      Der Universität Leipzig. Sie sind nach den
+      <a href="http://wortschatz.uni-leipzig.de/use.html">Wortschatz-Nutzungsbedingungen</a>
+      unter
+      <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC-BY-NC</a>
+      Lizenz verfügbar (gemäß den
+      <a href="http://wortschatz.uni-leipzig.de/use.html">Wortschatz Nutzungsbedingungen</a>).
+      Wörter, die andere Zeichen als Buchstaben enthalten und sehr kurze Wörter wurden herausgefiltert.
+      Außerdem wurden alle Wörter in Kleinschreibung umgewandelt.
+      </p>
+      <p>
+      Sie können diese Dateien verwenden, um Ihr Passwort lokal zu generieren.
+      Wenn Sie Linux verwenden, können Sie einfach den folgenden Befehl aufrufen
+      (&lt;file&gt; durch die entsprechende Datei ersetzen):
+      </p>''',
     'option_numberwords': 'Anzahl Wörter', 
     'option_showspaces': 'Trenne Wörter mit Leerzeichen',
     'options_need': 'Ich brauche:', 
